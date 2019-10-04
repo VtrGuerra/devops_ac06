c4056c95839bd17e968cbec7be6b3120c566358e
VtrGuerra <53881682+VtrGuerra@users.noreply.github.com>
2019-10-03 21:59:30 -0300

    erros3

--- com/joao/test/test_calcula_parcela.py
+++ com/joao/test/test_calcula_parcela.py
@@ -1,13 +1,6 @@
 from com.joao.calcula_parcela import valorPagamento
 
 
-def test_calcula_parcela():
-	assert valorPagamento(50, 0) == 50
-
-
-def test_dias_de_atraso():
-	assert valorPagamento(50, 2) == 52.5
-
-
-def test_dias():
-	assert valorPagamento(-1, 0) is None
+def test_calcula():
+    #calcula = calcula_parcela()
+    assert valorPagamento(0, 2) == 0
--- com/joao/test/test_conta_corrente.py
+++ com/joao/test/test_conta_corrente.py
@@ -1,14 +1,17 @@
 from com.joao.conta_corrente import ContaCorrente
 
+
 def test_alterarNome():
     conta = ContaCorrente(7,"Wallas")
     conta.alterarNome("Bradesco")
     assert conta.nomeCorrentista == "Bradesco"
 
+
 def test_alterardeposito():
     conta = ContaCorrente(7,"Wallas")
     conta.deposito(100)
     assert conta.saldo == 100
+
 
 def test_alterarsaque():
     conta = ContaCorrente(7,"Wallas", 100)
